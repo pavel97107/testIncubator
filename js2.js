f07b604eff205f9f1da5965117251eec71b90b2a
Pavel <pavel97107@gmail.com>
2020-05-07 13:43:44 +0300

    'AJAX,DELETE,PUT'

--- js2.js
+++ js2.js
@@ -1,18 +1,27 @@
-
-
 const btn = document.querySelector('button');
-const page = document.getElementById('page');
+const deleteBtn = document.getElementById('delete');
+const text = document.getElementById('text');
+
+
 
 btn.addEventListener('click', () => {
-    getImages(page)
-        .then(dataReceiv)
-})
+    createTasks(text.value.toString());
+    getTasks()
+        .then(data)
+});
 
 
-const dataReceiv = (array) => {
-        array.forEach((element) => {
-            const img = document.createElement('img');
-            img.src = element.original;
-            document.querySelector('body').appendChild(img);
-        })
+deleteBtn.addEventListener('click', () => {
+    updateTasks('learn JS', 'c0993095-3225-40d3-8747-ec7ff5d07666');
+});
+
+
+
+const data = (tasks) => {
+    tasks.forEach((task) => {
+        const li = document.createElement('li');
+        li.dataset.id = task.id;
+        li.textContent = task.title;
+        document.querySelector('body').appendChild(li);
+    })
 };
--- service.js
+++ service.js
@@ -7,3 +7,41 @@ const getImages = (page) => {
         return response.data;
     });
 };
+
+
+const getTasks = () => {
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=11117`);
+    return promise.then((response) => {
+        return response.data;
+    });
+};
+
+const createTasks = (title) => {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks`, {
+        widgetId: 11117,
+        title: title
+    });
+    return promise.then((response) => {
+        return response.data;
+    });
+
+};
+
+const updateTasks = (title, id) => {
+    const promise = axios.put(`https://repetitora.net/api/JS/Tasks`, {
+        widgetId: 11116,
+        title: title,
+        taskId: id
+    });
+    return promise.then((response) => {
+        return response.data;
+    });
+};
+
+const deleteTasks = (id) => {
+    const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=11116&taskId=${id}`);
+    return promise.then((response) => {
+        return response.data;
+    });
+};
+
